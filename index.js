c8a36b6e069577489c7c18407d9a8f25527c45f4
Cindy Qu <cindyqu42@gmail.com>
2022-08-05 01:53:31 -0700

    commit tree c38f34b656a4791716d6a3e5537a7b41c5d2df2b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const theList = document.getElementById("main");
+theList.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "YOUR-NAME is the champion";
